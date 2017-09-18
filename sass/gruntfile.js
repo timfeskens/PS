078ba1a83575bed4fbabbbc0ef20d64e86c1fbe0
Tim Feskens <TimFeskens@MacBook-Pro-Yo.lan>
2017-09-18 11:40:00 +0200

    fixed gruntfile to only use external tasks

--- sass/gruntfile.js
+++ sass/gruntfile.js
@@ -5,57 +5,11 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'myproject/src/<%= pkg.name %>.js',
-        dest: 'myproject/build/js/<%= pkg.name %>.min.js'
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['**/*.js', '**/*.scss'],
-        tasks: ['build'],
-        options: {
-          spawn: false,
-        },
-      },
-    },
-    cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'myproject/build/css/',
-          src: ['*.css', '!*.min.css'],
-          dest: 'myproject/build/css/',
-          ext: '.min.css'
-        }]
-      }
-    },
-    sass: {
-      options: {
-          sourceMap: true
-      },
-      dist: {
-          files: {
-              'myproject/build/css/style.css': 'myproject/src/sass/style.scss'
-          }
-      }
-  },
-  htmlmin: {                                     // Task
-    dist: {                                      // Target
-      options: {                                 // Target options
-        removeComments: true,
-        collapseWhitespace: true
-      },
-      files: {                                   // Dictionary of files
-        'myproject/index.html': 'myproject/big-index.html '   // 'destination': 'source'
-      }
-    }
-  }
+    
   });
+
+  var configs = require('load-grunt-configs')(grunt);
+  grunt.initConfig(configs);
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
