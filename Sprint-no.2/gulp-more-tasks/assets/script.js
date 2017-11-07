7ed3a2c410f3a74eab70278150e50998915729fe
Tim Feskens <timf_1998@hotmail.com>
2017-11-07 16:02:16 +0100

    first commit of gulp-more-tasks

---
+++ Sprint-no.2/gulp-more-tasks/assets/script.js
@@ -0,0 +1,1 @@
+function askQuestions(){var o=prompt("Hi, what's your name?"),a=prompt("Also, what's your favorite kind of pizza?"),s=prompt("Lastly, do you like dogs or cats?");alert(o+",\n \nyour favorite pizza is "+a+"\nand you like "+s)}askQuestions();
---
+++ Sprint-no.2/gulp-more-tasks/gulpfile.js
@@ -0,0 +1,23 @@
+// Gulp tasks
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+gulp.task('copy', function() {
+    gulp.src('index.html')
+    .pipe(gulp.dest('assets'))
+});
+
+gulp.task('sass', function() {
+    gulp.src('styles/style.scss')
+    .pipe(sass({style: 'expanded'}))
+    .pipe(gulp.dest('assets'))
+});
+
+gulp.task('uglify', function() {
+    gulp.src('scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('assets'))
+  });
+
+  gulp.task('default', ['uglify', 'sass']);
