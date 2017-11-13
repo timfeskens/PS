bb49982324098803025b8383313567b796073e89
Tim Feskens <timf_1998@hotmail.com>
2017-11-13 09:28:51 +0100

    added coments to make the code easier to understand

--- Sprint-no.2/largest-of-5/index.js
+++ Sprint-no.2/largest-of-5/index.js
@@ -1,12 +1,16 @@
 function makeRandomNumbers() {
+    // make the array
     var numberarrray = [];
-
+    //fill the array until there are 5 numbers in it
     for (i = 0; i <5; i++) {
+      // create a random number under 1
       var randomnumber = Math.random();
+      // multiplies the number and keeps it between 1 and 10
       var completenumber = Math.floor(randomnumber  * 9) + 1;
-  
+      // pushes the number in the array
       numberarrray.push(completenumber);
       
+      // fills the html with the data
       document.getElementById('textbox').innerHTML = 'the numbers are ' + numberarrray;
       document.getElementById('textbox').innerHTML += '<br>and the highest number is ' + Math.max(...numberarrray);
     }
